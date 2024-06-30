0f90b39a743679556ec530d626c5a0d74762c3b0
Tane4ka170 <tane4ka170@gmail.com>
2024-07-01 02:35:39 +0300

    Added content to pages

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,5 +1,36 @@
-const Category = () => {
-  return <div>Category</div>;
-};
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getFilteredSongs } from "../api/api";
+
+function Category() {
+  const { digit, letter } = useParams();
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    let queryParam = digit || letter;
+
+    getFilteredSongs(queryParam)
+      .then((sortedSongs) => {
+        sortedSongs.sort((a, b) => a.artist.localeCompare(b.artist));
+        setSongs(sortedSongs);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the songs!", error);
+      });
+  }, [digit, letter]);
+
+  return (
+    <div>
+      <h1>Songs starting with {digit || letter}</h1>
+      <ul>
+        {songs.map((song) => (
+          <li key={song._id}>
+            {song.artist} - {song.song}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default Category;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
-const Home = () => {
-  return <div>Home</div>;
-};
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Archive of Millenium</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/songs">View Songs</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
 
 export default Home;
