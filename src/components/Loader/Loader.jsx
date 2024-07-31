6177dccafa9afe657f0f7206102d342646d6626a
Tane4ka170 <tane4ka170@gmail.com>
2024-07-31 23:10:50 +0300

    fixed Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,15 +1,15 @@
-import { LoaderLines } from "./Loader.styled";
+import { LoaderContainer, LoaderLines } from "./Loader.styled";
 
 const Loader = () => {
   return (
-    <div>
+    <LoaderContainer>
       <LoaderLines
         color="#9ce3f9"
         width="100"
         visible={true}
         ariaLabel="falling-circles-loading"
       />
-    </div>
+    </LoaderContainer>
   );
 };
 
