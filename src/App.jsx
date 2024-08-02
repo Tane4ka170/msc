d5197e6bb15af1c6f9f4739d7a3649799d7fee58
Tane4ka170 <tane4ka170@gmail.com>
2024-08-02 15:32:20 +0300

    added lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,88 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import { Suspense, lazy, useEffect } from "react";
+import Loader from "./components/Loader/Loader";
+import { warmUpServer } from "./api/api";
 
-import Home from "./pages/Home/Home";
-import AllArchive from "./pages/AllArchive/AllArchive";
-import NumbersCategory from "./pages/Letters/NumbersCategory";
-import LatinCategory from "./pages/Letters/LatinCategory";
-import CyrillicCategory from "./pages/Letters/CyrillicCategory";
-import Category from "./pages/Category/Category";
+const Home = lazy(() => import("./pages/Home/Home"));
+const AllArchive = lazy(() => import("./pages/AllArchive/AllArchive"));
+const NumbersCategory = lazy(() => import("./pages/Letters/NumbersCategory"));
+const LatinCategory = lazy(() => import("./pages/Letters/LatinCategory"));
+const CyrillicCategory = lazy(() => import("./pages/Letters/CyrillicCategory"));
+const Category = lazy(() => import("./pages/Category/Category"));
 
 function App() {
+  useEffect(() => {
+    warmUpServer();
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="songs" element={<AllArchive />} />
-        <Route path="songs/numbers" element={<NumbersCategory />} />
-        <Route path="songs/latin" element={<LatinCategory />} />
-        <Route path="songs/cyrillic" element={<CyrillicCategory />} />
-        <Route path="songs/numbers/:digit" element={<Category />} />
-        <Route path="songs/latin/:letter" element={<Category />} />
-        <Route path="songs/cyrillic/:letter" element={<Category />} />
+        <Route
+          index
+          element={
+            <Suspense fallback={<Loader />}>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route
+          path="songs"
+          element={
+            <Suspense fallback={<Loader />}>
+              <AllArchive />
+            </Suspense>
+          }
+        />
+        <Route
+          path="songs/numbers"
+          element={
+            <Suspense fallback={<Loader />}>
+              <NumbersCategory />
+            </Suspense>
+          }
+        />
+        <Route
+          path="songs/latin"
+          element={
+            <Suspense fallback={<Loader />}>
+              <LatinCategory />
+            </Suspense>
+          }
+        />
+        <Route
+          path="songs/cyrillic"
+          element={
+            <Suspense fallback={<Loader />}>
+              <CyrillicCategory />
+            </Suspense>
+          }
+        />
+        <Route
+          path="songs/numbers/:digit"
+          element={
+            <Suspense fallback={<Loader />}>
+              <Category />
+            </Suspense>
+          }
+        />
+        <Route
+          path="songs/latin/:letter"
+          element={
+            <Suspense fallback={<Loader />}>
+              <Category />
+            </Suspense>
+          }
+        />
+        <Route
+          path="songs/cyrillic/:letter"
+          element={
+            <Suspense fallback={<Loader />}>
+              <Category />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
